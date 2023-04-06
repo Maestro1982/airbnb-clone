bfe322ce6a29aa81afcb299e5fb4a1a073478f84
Maestro1982 <kris.tote@telenet.be>
2023-04-06 12:47:31 +0200

    create CategoryBox.tsx and Categories.tsx components, modify LoginModal.tsx, Logo.tsx and Navbar.tsx

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -91,12 +91,12 @@ const LoginModal = () => {
       />
       <div className='text-neutral-500 text-center mt-4 font-light'>
         <div className='flex flex-row justify-center items-center gap-2'>
-          <div>Already have an account?</div>
+          <div>No account?</div>
           <div
-            onClick={registerModal.onClose}
+            onClick={loginModal.onClose}
             className='text-neutral-800 cursor-pointer hover:underline'
           >
-            Login
+            Sign Up
           </div>
         </div>
       </div>
