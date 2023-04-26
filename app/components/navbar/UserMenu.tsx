c9fa72fec62b68ff82c60086ed8a970dbdd2291b
Maestro1982 <kris.tote@telenet.be>
2023-04-26 16:24:51 +0200

    create getReservations.ts, route.ts in reservations folder, page.tsx trips folder, TripsClient.tsx in trips folder, modify ListingCard.tsx, UserMenu.tsx, ListingClient.tsx, page.tsx listings folder [listingId], index.ts in types

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -8,12 +8,14 @@ import useRegisterModal from '@/app/hooks/useRegisterModal';
 import useLoginModal from '@/app/hooks/useLoginModal';
 import { SafeUser } from '@/app/types';
 import useRentModal from '@/app/hooks/useRentModal';
+import { useRouter } from 'next/navigation';
 
 interface UserMenuProps {
   currentUser?: SafeUser | null;
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
+  const router = useRouter();
   const [isOpen, setIsOpen] = useState(false);
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
@@ -55,7 +57,10 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
           <div className='flex flex-col cursor-pointer'>
             {currentUser ? (
               <>
-                <MenuItem onClick={() => {}} label='My trips' />
+                <MenuItem
+                  onClick={() => router.push('/trips')}
+                  label='My trips'
+                />
                 <MenuItem onClick={() => {}} label='My favorites' />
                 <MenuItem onClick={() => {}} label='My reservations' />
                 <MenuItem onClick={() => {}} label='My properties' />
